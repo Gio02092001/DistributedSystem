460d2b7971962993a17f353161ea4c032761907b
Gio02092001 <79279167+Gio02092001@users.noreply.github.com>
2021-04-23 16:19:49 +0200

    Delete DistributedSystems1 - Kopie directory

--- DistributedSystems1 - Kopie/src/main/java/com/distributedsystems/task/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.distributedsystems.task.repository;
-
-import com.distributedsystems.task.model.Order;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends CrudRepository<Order,Integer> {
-}
